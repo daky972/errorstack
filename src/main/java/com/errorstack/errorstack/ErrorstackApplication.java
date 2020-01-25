9a0db8790d0e25c02f307bafb9bbf7e1b8083011
dsantor <dsantor@eryce.com>
2020-01-25 13:46:42 +0100

    init

---
+++ src/main/java/com/errorstack/errorstack/ErrorstackApplication.java
@@ -0,0 +1,13 @@
+package com.errorstack.errorstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/errorstack/errorstack/ErrorstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errorstack.errorstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
